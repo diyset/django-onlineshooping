317e5c4360ca1e4abf7ef3a0ba2f5a5a59f67672
Dian Setiyadi <diyansetiyadi@gmail.com>
2018-12-03 10:24:14 +0700

    catalog product done and added chart for shop chart

--- onlineshooping/settings.py
+++ onlineshooping/settings.py
@@ -121,3 +121,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+FILE_UPLOAD_PERMISSIONS = 0o644
+
+CART_SESSION_ID = 'cart'

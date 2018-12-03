57f787aa8f7523e672f9d7bcf5c915150cd93fa4
Dian Setiyadi <diyansetiyadi@gmail.com>
2018-12-03 10:53:56 +0700

    create function cart session

--- onlineshooping/settings.py
+++ onlineshooping/settings.py
@@ -37,7 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # Local
     'shop.apps.ShopConfig',
+    'chart.apps.ChartConfig',
 
 ]
 

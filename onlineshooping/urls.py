d4185d3b6b3c56fe313ef44c59cd4dd5626e456e
Dian Setiyadi <diyansetiyadi@gmail.com>
2018-12-06 01:40:16 +0700

    checkout template

--- onlineshooping/urls.py
+++ onlineshooping/urls.py
@@ -21,6 +21,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('cart/', include('chart.urls', namespace='cart')),
+    path('orders/', include('orders.urls', namespace='orders')),
     path('', include('shop.urls', namespace='shop')),
 
 ]
